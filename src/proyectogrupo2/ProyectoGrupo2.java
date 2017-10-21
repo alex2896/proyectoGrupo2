e794e3b908606aa080af4cf4269c634c097a76dc
danie <danie@DESKTOP-7ISGMNI>
2017-10-20 22:49:28 -0500

    commit dp 2

--- src/proyectogrupo2/ProyectoGrupo2.java
+++ src/proyectogrupo2/ProyectoGrupo2.java
@@ -16,5 +16,6 @@ public class ProyectoGrupo2 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println(".l.");
+        System.out.println(".l..");
     }
 }
